bf58405f3831be01616f4e401cc2d5283894d471
Konstantin <turbo.foxy01@gmail.com>
2024-05-02 21:30:17 +0200

    view mechanic added for the guard

--- Scripts/Character Scripts/military man.gd
+++ Scripts/Character Scripts/military man.gd
@@ -1,19 +1,32 @@
 extends CharacterBody2D
 @export var speed = 50
-
+@export var view_length = 200
+@export var view_angle = 90
 var time :=0.0
 var previous_coordinates : Vector2 =global_position
+var direction
+var stop = false
 func _ready():
 	$animation.play("idle")
-
+	$VisionCone2D.angle_deg = view_angle
+	$VisionCone2D.max_distance = view_length
 var move_left = previous_coordinates.x-global_position.x>0
 func _process(delta):
 	
+	
 	time+=delta
-	if $"..".name == "follow path":
+	if $"..".name == "follow path" and not stop:
 		$"..".progress = time*speed
+	print(direction)
+
+	
 	$"..".rotation_degrees = 0
+	
 	$".".rotation_degrees = 0
+	#if move_left and $VisionCone2D.rotation_degrees<0:
+		#$VisionCone2D.rotation_degrees*=-1
+	#elif not move_left and $VisionCone2D.rotation_degrees>0:
+		#$VisionCone2D.rotation_degrees*=-1
 	move_left = previous_coordinates.x-global_position.x>0
 	#play animation
 	
@@ -21,9 +34,13 @@ func _process(delta):
 		$animation.play("walk")
 		if move_left and scale.x >0:
 			scale.x *=-1
+			$VisionCone2D.rotation_degrees*=-1
+			
 			
 		if not move_left and scale.x<0:
 			scale.x*=-1
+			$VisionCone2D.rotation_degrees*=-1
+			
 	else:
 		$animation.play("idle")
 	previous_coordinates = global_position
@@ -35,4 +52,17 @@ func _process(delta):
 func _on_detection_body_entered(body):
 	if body.name == "Main character":
 		if body.not_in_view == false:
+			stop = true
 			body.death()
+			
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Main character":
+		if body.not_in_view == false:
+			stop = true
+			body.death()
+	
+
+
+
